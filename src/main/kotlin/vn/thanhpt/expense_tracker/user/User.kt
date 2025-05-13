8da14c3dba4cf7835fdb71a69bd4950e27289a5c
thanh.pt1 <thanh.pt1@kiotviet.com>
2025-05-13 17:46:12 +0700

    add generatedValue anottion to user

--- src/main/kotlin/vn/thanhpt/expense_tracker/user/User.kt
+++ src/main/kotlin/vn/thanhpt/expense_tracker/user/User.kt
@@ -1,13 +1,12 @@
 package vn.thanhpt.expense_tracker.user
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
-import jakarta.persistence.Table
 
 @Entity
-@Table(name = "users")
 data class User(
-        @Id val id: String = "",
+        @Id @GeneratedValue() val id: String = "",
         val name: String = "",
         val email: String = "",
         val phone: String = ""
